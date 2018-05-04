41f0a8ab72ea4f6daa77015f830d4c4e305f6d91
なるみ <weaper@gmail.com>
2018-05-04 16:16:56 +0800

    migrate to tweepy

--- main.py
+++ main.py
@@ -1,56 +1,64 @@
+import argparse
 import json
+import logging
 import os
+from time import sleep
 
-import twitter
+import tweepy
+
+
+def read_json(file):
+    with open(file, 'r') as fp:
+        return json.load(fp)
 
 
 class TwitterHelper(object):
 
-    def __init__(self, path):
+    def __init__(self, auth_file):
         """
         Args:
-            path: A path of config file.
+            auth_file: A file path of auth config.
         """
-        if not os.path.exists(path):
-            raise FileNotFoundError
+        self.api = self._get_api(auth_file)
+        self.last_id = 0
+        self.logger = logging.getLogger('Twitter Helper')
 
-        with open(path, 'r') as fp:
-            self.config = json.load(fp)
+    def _get_api(self, auth_path):
+        auth_dict = read_json(auth_path)
+        auth = tweepy.OAuthHandler(auth_dict['consumer_key'],
+                                   auth_dict['consumer_secret'])
+        auth.set_access_token(auth_dict['access_token_key'],
+                              auth_dict['access_token_secret'])
+        return tweepy.API(auth, wait_on_rate_limit=True)
 
-        self.api = twitter.Api(**self.config)
+    def run(self, query, lang=None):
+        while True:
+            try:
+                tweets = self.api.search(
+                    query, lang, since_id=self.last_id, tweet_mode='extended')
+            except tweepy.error.TweepError as e:
+                print(e)
+                continue
 
-    def get_search(self, **kwargs):
-        statuses = self.api.GetSearch(**kwargs)
+            for tweet in tweets:
+                if isinstance(tweet, tweepy.models.Status):
+                    # print(tweet._json['full_text'])
+                    print(tweet._json['created_at'])
 
-        res = []
-        for status in statuses:
-            if isinstance(status, twitter.models.Status):
-                res.append(
-                    json.dumps(status._json, indent=4, ensure_ascii=False))
-
-        return res
-
-    def get_trends(self):
-        trends = self.api.GetTrendsCurrent()
-        res = []
-
-        for trend in trends:
-            if isinstance(trend, twitter.models.Trend):
-                res.append(
-                    json.dumps(trend._json, indent=4, ensure_ascii=False))
-
-        return res
+                    cur_id = int(tweet._json['id'])
+                    self.last_id = max(self.last_id, cur_id)
 
 
 def main():
-    tc = TwitterHelper('config.json')
-    statuses = tc.get_search(term='#Taiwan', lang='ja')
-    for status in statuses:
-        print(status)
+    """
+    $ python main.py -q "#NobelPrize"
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--query', '-q', type=str)
+    args = parser.parse_args()
 
-    trends = tc.get_trends()
-    for trend in trends:
-        print(trend)
+    th = TwitterHelper('auth.json')
+    th.run(args.query)
 
 
 if __name__ == '__main__':
